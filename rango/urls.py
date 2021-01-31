4e8ff50aa11c80b69c3b85aee354ead4f82d27b4
Fadi Chahine <2483052c@student.gla.ac.uk>
2021-01-31 18:42:59 +0400

    Chapter 3 Exercises

--- rango/urls.py
+++ rango/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from rango import views
 
+
 app_name = 'rango'
 
 urlpatterns = [
 
-    path('', views.index, name = 'index')
+path('', views.index, name = 'index'),
+
+path('about/', views.about, name = 'about')
 
 ]
