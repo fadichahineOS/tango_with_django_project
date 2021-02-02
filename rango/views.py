d737decb01b1d705ea242101df5f32244b8d7e87
Fadi Chahine <2483052c@student.gla.ac.uk>
2021-02-02 13:19:41 +0400

    Chapter 4 Exercises

--- rango/views.py
+++ rango/views.py
@@ -15,9 +15,10 @@ def index(request):
 
     return render(request, 'rango/index.html', context=context_dict)
     
-    return HttpResponse('Rango says hey there partner! <a href="/rango/about/">About</a>.')
 
 def about(request):
-    return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
+
+
+    return render(request, 'rango/about.html')
 
 
