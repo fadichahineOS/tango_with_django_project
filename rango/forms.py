8218c27adf7b71c30e1d4476a80441751b788d9d
Fadi Chahine <2483052c@student.gla.ac.uk>
2021-02-08 16:38:34 +0400

    Chapter 9 Reading

--- rango/forms.py
+++ rango/forms.py
@@ -1,7 +1,8 @@
 from django import forms
-from rango.models import Page, Category
+from django.contrib.auth.models import User
+from rango.models import Page, Category, UserProfile
 
-# We could add these forms to views.py, but it makes sense to split them off into their own file.
+
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
@@ -30,4 +31,16 @@ class PageForm(forms.ModelForm):
             url = f'http://{url}'
             cleaned_data['url'] = url
 
-        return cleaned_data 
+        return cleaned_data
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture',) 
